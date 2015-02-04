3ac7a97d6741bd196b0c803cf9cd9fa0454f84d3
felipesudrj <felipe.devel@gmail.com>
2015-02-04 16:21:14 -0200

    Update header.php

--- theme/header.php
+++ theme/header.php
@@ -6,7 +6,7 @@
  *
  * @package WordPress
  * @subpackage Twenty_Twelve
- * @since Twenty FelTh 1.0
+ * @since Twenty Twelve 1.0
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
@@ -32,20 +32,48 @@
 </head>
 
 <body <?php body_class(); ?>>
- <div class="templatemo-top-bar" id="templatemo-top">
+<div id="page">
+	 <div class="templatemo-top-bar" id="templatemo-top">
             <div class="container">
                 <div class="subheader">
                     <div id="phone" class="pull-left">
                             <img src="images/phone.png" alt="phone"/>
-                            010-020-0340
+                            NNNUMMME
                     </div>
                     <div id="email" class="pull-right">
                             <img src="images/email.png" alt="email"/>
-                            contact@website.com
+                            email@email.com
                     </div>
                 </div>
             </div>
         </div>
-       
-
-	<div id="main" class="wrapper">
+       <!-- NAV -->
+		<div class="templatemo-top-menu">
+            <div class="container">
+                <!-- Static navbar -->
+                <div class="navbar navbar-default" role="navigation">
+                    <div class="container">
+                        <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                </button>
+                                <a href="#" class="navbar-brand"><img src="images/logo.jpg" alt="Sua Logo" title="Sua Logo" width="100px;" height="100px;"/></a>
+                        </div>
+                        <div class="navbar-collapse collapse" id="templatemo-nav-bar">
+                            <ul class="nav navbar-nav navbar-right" style="margin-top: 40px;">
+                                <li class="active"><a href="#templatemo-top">Home</a></li>
+                                <li><a href="#templatemo-welcome">Nossa empresa</a></li>
+                                <li><a href="#templatemo-portfolio">Produtos</a></li>
+                                <li><a href="#templatemo-blog">Depoimentos</a></li>
+                                <li><a rel="nofollow" href="http://www.google.com" class="external-link">Outros links</a></li>
+                                <li><a href="#templatemo-contact">Contato</a></li>
+                            </ul>
+                        </div><!--/.nav-collapse -->
+                    </div><!--/.container-fluid -->
+                </div><!--/.navbar -->
+            </div> <!-- /container -->
+        </div>
+        
